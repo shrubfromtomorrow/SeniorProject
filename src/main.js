b60d0125fb820a48fb16a15adc8c95e8c5bdd026
shrubfromtomorrow <orionjandrews@gmail.com>
2022-11-26 21:04:49 -0800

    Small commit. Restructured main.js to initiate mathquill before rest of code to allow dom manipulation. Need to figure out how to find current value of all text in input box, textarea doesn't actually hold any value

--- src/main.js
+++ src/main.js
@@ -1,28 +1,5 @@
 
-const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const otherKeys = [" "];
-let latestInputString = "";
-
-document.onkeydown = function (event) {
-    if (event.key == "r" && event.ctrlKey == true) {
-        return;
-    }
-    else if (letters.includes(event.key)) {
-        event.preventDefault();
-    }
-    else if (event.key == "Backspace") {
-        latestInputString = latestInputString.substring(0, latestInputString.length - 1);
-        console.log(latestInputString);
-    }
-    else {
-        latestInputString += event.key;
-        console.log(latestInputString);
-    }
-}
-
 var MQ = MathQuill.getInterface(2);
-var problemSpan = document.getElementById('problem');
-MQ.StaticMath(problemSpan);
 var answerSpan = document.getElementById('answer');
 var answerMathField = MQ.MathField(answerSpan, {
     handlers: {
@@ -32,3 +9,25 @@ var answerMathField = MQ.MathField(answerSpan, {
         }
     }
 });
+
+
+let answerBox = document.getElementById("answer").firstChild.firstChild;
+answerBox.focus();
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const otherKeys = [" "];
+
+document.onkeydown = function (event) {
+    answerBox.focus();
+    if (event.key == "r" && event.ctrlKey == true) {
+        return;
+    }
+    else if (event.key == "Enter") {
+        console.log(rootBlock.value);
+    }
+    else if (letters.includes(event.key)) {
+        event.preventDefault();
+    }
+    else {
+        console.log(event.key);
+    }
+}
