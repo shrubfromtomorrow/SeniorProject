454707a353e86ff6d3599dde53a979344c8aecb3
shrubfromtomorrow <orionjandrews@gmail.com>
2022-11-27 21:13:26 -0800

    Properly selected all chilren of 'textbox', need to delete all children on enter but indexing removes only first x children where x initial number of children

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ var answerMathField = MQ.MathField(answerSpan, {
 
 let answerBox = document.getElementById("answer").firstChild.firstChild;
 answerBox.focus();
+
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const otherKeys = [" "];
 
@@ -22,7 +23,16 @@ document.onkeydown = function (event) {
         return;
     }
     else if (event.key == "Enter") {
-        console.log(rootBlock.value);
+        let rootBlockChildren = document.getElementById("answer").children[1].children;
+        let rootBlockChildrenValues = [];
+        for (let i = 0; i < rootBlockChildren.length - 1; i++) {
+            rootBlockChildrenValues.push(rootBlockChildren[i].textContent);
+        }
+        console.log(rootBlockChildrenValues);
+        console.log(rootBlockChildren.length);
+        for (let i = 0; i < rootBlockChildren.length; i++) {
+            rootBlockChildren[0].remove();
+        }
     }
     else if (letters.includes(event.key)) {
         event.preventDefault();
